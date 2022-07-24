ee5c1a8aca4ffbc112207df47968a8d2287312f7
Lex <webdevlex35@gmail.com>
2022-07-24 10:19:15 -0400

    update first-num component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstNumComponent } from './first-num/first-num.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstNumComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/calc.ts
@@ -0,0 +1,3 @@
+
+
+let firstNum: string;
