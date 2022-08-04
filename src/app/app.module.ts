44076cb728c1da76d057c235c53656a574295b48
Lex <webdevlex35@gmail.com>
2022-08-04 19:06:53 -0400

    updated user interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FirstNumComponent } from './first-num/first-num.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstNumComponent
   ],
   imports: [
     BrowserModule,
